137b02bd63f38c4c217e3778c34043bee19cc189
pythononpractice <123281994+pythononpractice@users.noreply.github.com>
2023-12-05 23:16:13 +0500

    Добавил путь до кнопки закрытия видео рекламы

--- pathes.py
+++ pathes.py
@@ -1,3 +1,4 @@
+main_banner = '/html/body/div[2]/div/div/div[3]/div[1]/div/div/div/div[2]'
 cookie_banner = '/html/body/div[2]/div/div/div[3]/footer/div[2]'
 items_count = '/html/body/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div/div[1]/header/div[3]/div/div[1]/div/div/a/span'
 main_block = '/html/body/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div/div[2]/div[2]/div[1]/div/div/div/div[2]/div'
